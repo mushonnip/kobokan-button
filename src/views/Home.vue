9908283be9bd9fdee515ad02d500d84ce416eda7
mushonnip <mushonnipabu@gmail.com>
2022-04-17 07:33:28 +0700

    add audio player

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,32 @@
+<script setup>
+import VoiceList from "../assets/generated-voice-list-id.json";
+import { NCard, NButton, NSpace } from "naive-ui";
+import { useI18n } from "vue-i18n";
+const voices = VoiceList.voices;
+const play = (category_path, item) => {
+  let audio = new Audio("voices/" + category_path + "/" + item.path);
+  audio.play();
+};
+const { locale } = useI18n();
+</script>
+
 <template>
-  <h1>Home</h1>
+  <div v-for="category in voices" v-bind:key="category.categoryName">
+    <n-card :title="category.categoryName">
+      <n-space>
+        <n-button
+          round
+          strong
+          secondary
+          type="info"
+          v-for="voiceItem in category.voiceList"
+          v-bind:key="voiceItem.name"
+          @click="play(category.categoryPath, voiceItem)"
+        >
+          {{ voiceItem.name }}
+        </n-button></n-space
+      >
+    </n-card>
+  </div>
 </template>
+<script></script>
